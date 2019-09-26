990bfe90350cd1685bfb2e04ccdaf626301f58cb
Antony Denyer <git@antonydenyer.co.uk>
2019-09-26 14:48:46 +0100

    build: store variables using encrypted travis ci
    upload files against release

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,12 +2,14 @@
 
 set -eo pipefail
 
-VERSION=${TRAVIS_BRANCH#"release/"}
+VERSION=${TRAVIS_BRANCH#"release/v"}
 
-export PREVIOUS_RELEASE=$(curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" -s https://api.github.com/repos/web3j/web3j/releases/latest | jq -r '.target_commitish' )
+export PREVIOUS_RELEASE=$(curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" -s https://api.github.com/repos/web3j/web3j-cli/releases/latest | jq -r '.target_commitish' )
 export CHANGELOG=$(git rev-list --format=oneline --abbrev-commit --max-count=50 ${PREVIOUS_RELEASE}..HEAD | jq --slurp --raw-input . )
 
-echo "Creating a new release on GitHub"
+echo "Creating a new release on GitHub with changes"
+echo -e "\n${CHANGELOG:1:-1}"
+
 API_JSON="{
   \"tag_name\": \"v${VERSION}\",
   \"target_commitish\": \"$(git rev-parse HEAD)\",
@@ -18,7 +20,12 @@ API_JSON="{
 }"
 
 
+export RESULT=$(curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" --data "$API_JSON" -s https://api.github.com/repos/web3j/web3j-cli/releases)
+export UPLOAD_URL=$(echo ${RESULT} | jq -r ".upload_url")
+
 for FILE in `find ./build/distributions -type f -name "web3j.*"`;
 do
   curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" -s "${UPLOAD_URL:0:-13}?name=$(basename -- $FILE)" -H "Content-Type: $(file -b --mime-type $FILE)" --data-binary @"${FILE}"
 done
+
+echo "Release finished"

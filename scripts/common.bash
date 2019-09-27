9f61b2ec20291d48638ab61e7f5a853918f9b4e5
Antony Denyer <email@antonydenyer.co.uk>
2019-09-27 10:45:16 +0100

    build: use a version to release

---
+++ scripts/common.bash
@@ -0,0 +1,20 @@
+set -eo pipefail
+
+[[ "$TRACE" ]] && set -x
+
+configure_github_user() {
+    git config --global user.email "git@antonydenyer.co.uk"
+    git config --global user.name "Antony Denyer"
+}
+
+github_clone() {
+    git clone https://antonydenyer:${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/web3j/$1.git
+    cd $1
+}
+
+ensure_version() {
+    if [[ "$VERSION" = "" ]]; then
+        echo "ERROR: Missing VERSION specify it using an env variable"
+        exit 1
+    fi
+}
--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -2,7 +2,22 @@
 
 set -eo pipefail
 
-VERSION=${TRAVIS_BRANCH#"release/v"}
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+export SCRIPTS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+source "$SCRIPTS_DIR/common.bash"
+
+ensure_version
 
 export PREVIOUS_RELEASE=$(curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" -s https://api.github.com/repos/web3j/web3j-cli/releases/latest | jq -r '.target_commitish' )
 export CHANGELOG=$(git rev-list --format=oneline --abbrev-commit --max-count=50 ${PREVIOUS_RELEASE}..HEAD | jq --slurp --raw-input . )
@@ -23,7 +38,7 @@ API_JSON="{
 export RESULT=$(curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" --data "$API_JSON" -s https://api.github.com/repos/web3j/web3j-cli/releases)
 export UPLOAD_URL=$(echo ${RESULT} | jq -r ".upload_url")
 
-for FILE in `find ./build/distributions -type f -name "web3j.*"`;
+for FILE in `find ./build/distributions -type f -name "web3j-${VERSION}.zip"`;
 do
   curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" -s "${UPLOAD_URL:0:-13}?name=$(basename -- $FILE)" -H "Content-Type: $(file -b --mime-type $FILE)" --data-binary @"${FILE}"
 done

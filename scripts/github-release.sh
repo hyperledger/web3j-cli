8c9a9e210a52e939a4223313f555ac739b096582
Antony Denyer <git@antonydenyer.co.uk>
2019-10-01 16:01:17 +0100

    release: zip and tar versions to github

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -19,7 +19,9 @@ source "$SCRIPTS_DIR/common.bash"
 
 ensure_version
 
-export PREVIOUS_RELEASE=$(curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" -s https://api.github.com/repos/web3j/web3j-cli/releases/latest | jq -r '.target_commitish' )
+export PRODUCT=web3j-cli
+
+export PREVIOUS_RELEASE=$(curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" -s https://api.github.com/repos/web3j/${PRODUCT}/releases/latest | jq -r '.target_commitish' )
 export CHANGELOG=$(git rev-list --format=oneline --abbrev-commit --max-count=50 ${PREVIOUS_RELEASE}..HEAD | jq --slurp --raw-input . )
 
 echo "Creating a new release on GitHub with changes"
@@ -35,10 +37,10 @@ API_JSON="{
 }"
 
 
-export RESULT=$(curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" --data "$API_JSON" -s https://api.github.com/repos/web3j/web3j-cli/releases)
+export RESULT=$(curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" --data "$API_JSON" -s https://api.github.com/repos/web3j/${PRODUCT}/releases)
 export UPLOAD_URL=$(echo ${RESULT} | jq -r ".upload_url")
 
-for FILE in `find ./build/distributions -type f -name "web3j-${VERSION}.zip"`;
+for FILE in `find ./build/distributions -type f -name "web3j-${VERSION}.*"`;
 do
   curl -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" -s "${UPLOAD_URL:0:-13}?name=$(basename -- $FILE)" -H "Content-Type: $(file -b --mime-type $FILE)" --data-binary @"${FILE}"
 done

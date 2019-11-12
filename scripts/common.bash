390b4fd6d7cfe25f3e3c18264248df2ac704fae3
Antony Denyer <git@antonydenyer.co.uk>
2019-11-12 12:43:03 +0000

    build: ensure version before deployment

--- scripts/common.bash
+++ scripts/common.bash
@@ -13,6 +13,10 @@ github_clone() {
 }
 
 ensure_version() {
+    if [[ -z "$VERSION" ]]; then
+        VERSION="${TRAVIS_BRANCH//release\/}"
+    fi
+
     if [[ "$VERSION" = "" ]]; then
         echo "ERROR: Missing VERSION specify it using an env variable"
         exit 1

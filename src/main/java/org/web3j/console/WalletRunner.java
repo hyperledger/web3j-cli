53627cd167b4b7185024158e20b6faa65c9e67a2
alexandrou <alexandrosrousos@gmail.com>
2020-03-09 10:33:59 +0000

    Removed web3j audit functionality. Removed web3j account functionality. Removed web3j fund functionality. Removed unused methods in InteractiveOptions.

--- src/main/java/org/web3j/console/WalletRunner.java
+++ src/main/java/org/web3j/console/WalletRunner.java
@@ -18,7 +18,7 @@ import static org.web3j.utils.Collection.tail;
 
 /** Class for managing our wallet command line utilities. */
 public class WalletRunner {
-    private static final String USAGE = "wallet create|update|send|fromkey|fund";
+    private static final String USAGE = "wallet create|update|send|fromkey";
 
     public static void run(String[] args) {
         main(args);
@@ -40,9 +40,6 @@ public class WalletRunner {
                     break;
                 case "fromkey":
                     KeyImporter.main(tail(args));
-                    break;
-                case "fund":
-                    WalletFunder.main(new ConsoleDevice(), tail(args));
                     break;
                 default:
                     Console.exitError(USAGE);
--- src/main/java/org/web3j/console/project/InteractiveOptions.java
+++ src/main/java/org/web3j/console/project/InteractiveOptions.java
@@ -13,14 +13,10 @@
 package org.web3j.console.project;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.Scanner;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import org.web3j.account.LocalWeb3jAccount;
 import org.web3j.console.project.utils.InputVerifier;
 
 import static java.io.File.separator;
@@ -115,20 +111,6 @@ public class InteractiveOptions {
                 : Optional.of(outputPath);
     }
 
-    public static Optional<String> setGeneratedTestLocationKotlin() {
-        print(
-                "Where would you like to save your tests ["
-                        + String.join(
-                                separator, System.getProperty("user.dir"), "src", "test", "kotlin")
-                        + "]");
-        String outputPath = getUserInput();
-        return outputPath.isEmpty()
-                ? Optional.of(
-                        String.join(
-                                separator, System.getProperty("user.dir"), "src", "test", "kotlin"))
-                : Optional.of(outputPath);
-    }
-
     public static boolean userWantsTests() {
         print("Would you like to generate unit test for your solidity contracts [Y/n] ? ");
         String userAnswer = getUserInput();
@@ -152,25 +134,5 @@ public class InteractiveOptions {
         print("Looks like the project exists. Would you like to overwrite it [y/N] ?");
         String userAnswer = getUserInput();
         return userAnswer.toLowerCase().equals("y");
-    }
-
-    public static boolean userHasWeb3jAccount() throws IOException {
-        if (LocalWeb3jAccount.configExists()) {
-            ObjectNode objectNode = LocalWeb3jAccount.readConfigAsJson();
-            return LocalWeb3jAccount.loginTokenExists(objectNode);
-        }
-        return false;
-    }
-
-    public static boolean configFileExists() {
-        return LocalWeb3jAccount.configExists();
-    }
-
-    public static boolean userWantsWeb3jAccount() throws IOException {
-
-        print("It looks like you don’t have a Web3j account, would you like to create one?");
-        print("This will provide free access to the Ethereum network [Y/n]");
-        String userAnswer = getUserInput();
-        return userAnswer.toLowerCase().equals("y") || userAnswer.trim().equals("");
     }
 }
